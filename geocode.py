367334d9f3c83ac18b4cf634fd8f79e279249ee0
skmullins <skmllns@gmail.com>
2015-12-15 10:39:55 -0500

    Writes to file.

--- geocode.py
+++ geocode.py
@@ -2,23 +2,31 @@ import csv
 
 from geopy.geocoders import Nominatim
 
-file_name = "FA2015.csv"
-
+r_file_name = "FA2015.csv"
+w_file_name = "LL_FA2015.csv"
 ''' 
 headers = id, prog, class, major, sex, age, addr_line1, addr_line2, addr_line3, 
           city, st, zip, country, race, hispanic
 '''
 
-with open(file_name) as f:
-   reader = csv.reader(f)
-   #skip headers
-   reader.next()
-   for row in reader:
-      if "UNITED STATES" in row[12]:
-         address = row[6] + " " + row[7] + " " + row[8] + " " + row[9] + " " + row[10] + " " + row[11]
-         geolocator = Nominatim()
-         location = geolocator.geocode(address)
-         if location:
-            print((location.latitude, location.longitude))     
-      else:
-         print "other country"
+rf = open(r_file_name, 'rb')
+wf = open(w_file_name, 'wb')
+
+reader = csv.reader(rf)
+writer = csv.writer(wf)
+
+reader.next()
+
+for row in reader:
+   for elem in row:
+      elem = elem.lstrip().rstrip()
+   if "UNITED STATES" in row[12]:
+      address = row[6] + " " + row[7] + " " + row[8] + " " + row[9] + " " + row[10] + " " + row[11]
+      geolocator = Nominatim()
+      location = geolocator.geocode(address)
+      if location:
+         print location
+         new_row = row + [location.latitude, location.longitude]
+         writer.writerow(new_row)
+   else:
+      print "other country"
